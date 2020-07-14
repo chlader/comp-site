c2b01b9f946689034ef5f672c2e5b85ac1feb12f
chlader <ryanchladek96@gmail.com>
2020-07-13 22:22:48 -0500

    almost done with about page

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,13 +1,29 @@
 <template>
-  <p>about page here</p>
+  <div style="display: flex; justify-content: space-around;">
+    <v-sheet width="40%" height="80%">
+      <v-img src="../assets/stock-pro-pic.jpg"></v-img>
+      <span>Ryan Chladek</span>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. In placerat nec turpis vulputate euismod. Etiam tempus iaculis lorem, in aliquam tortor. Vivamus condimentum euismod nisl a hendrerit. Fusce euismod odio a quam sagittis feugiat. Aenean non neque tempus, placerat est eu, convallis lectus. Suspendisse potenti. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Aliquam erat volutpat.
+        Sed tincidunt tempor convallis. Proin quis fermentum nunc. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Sed quis ex elit. Ut sed pulvinar mi, ut ultrices augue. Cras rutrum dignissim justo. Aliquam et mi dui. Phasellus erat lectus, commodo vitae quam quis, rhoncus tincidunt felis. Nunc quis ex non lacus tristique placerat porta nec ex. Nam mauris neque, condimentum in velit ac, fermentum auctor urna. Suspendisse eget mauris vitae libero porta commodo. Vivamus interdum cursus lacus, nec vestibulum augue vulputate quis. Vivamus efficitur nibh enim, sit amet convallis urna luctus vitae.
+        Aenean pellentesque cursus nisi quis lacinia. Aliquam et varius mauris, sed aliquam enim. Sed vulputate est purus, a iaculis erat laoreet at. Vestibulum tempus efficitur ligula, a gravida sapien dapibus non. In lorem arcu, venenatis ut risus vitae, vulputate mattis lacus. Morbi accumsan nulla eu turpis pulvinar, sit amet pretium dolor auctor. Cras aliquet tellus eget pulvinar imperdiet. Maecenas at lectus urna. Integer molestie eget dui et porta. Donec sapien diam, posuere a volutpat at, tempor vel dui.
+      </p>
+    </v-sheet>
+    <v-sheet width="40%">
+      <v-img src="../assets/stock-pro-pic.jpg"></v-img>
+      <span>Ryan Chladek</span>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. In placerat nec turpis vulputate euismod. Etiam tempus iaculis lorem, in aliquam tortor. Vivamus condimentum euismod nisl a hendrerit. Fusce euismod odio a quam sagittis feugiat. Aenean non neque tempus, placerat est eu, convallis lectus. Suspendisse potenti. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Aliquam erat volutpat.
+        Sed tincidunt tempor convallis. Proin quis fermentum nunc. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Sed quis ex elit. Ut sed pulvinar mi, ut ultrices augue. Cras rutrum dignissim justo. Aliquam et mi dui. Phasellus erat lectus, commodo vitae quam quis, rhoncus tincidunt felis. Nunc quis ex non lacus tristique placerat porta nec ex. Nam mauris neque, condimentum in velit ac, fermentum auctor urna. Suspendisse eget mauris vitae libero porta commodo. Vivamus interdum cursus lacus, nec vestibulum augue vulputate quis. Vivamus efficitur nibh enim, sit amet convallis urna luctus vitae.
+        Aenean pellentesque cursus nisi quis lacinia. Aliquam et varius mauris, sed aliquam enim. Sed vulputate est purus, a iaculis erat laoreet at. Vestibulum tempus efficitur ligula, a gravida sapien dapibus non. In lorem arcu, venenatis ut risus vitae, vulputate mattis lacus. Morbi accumsan nulla eu turpis pulvinar, sit amet pretium dolor auctor. Cras aliquet tellus eget pulvinar imperdiet. Maecenas at lectus urna. Integer molestie eget dui et porta. Donec sapien diam, posuere a volutpat at, tempor vel dui.
+      </p>
+    </v-sheet>
+  </div>
 </template>
 
 <script>
-export default {
-
-}
+export default {};
 </script>
 
 <style>
-
 </style>
